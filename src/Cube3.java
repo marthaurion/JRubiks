178102fe49471f965fbd52b3971a396c508907f1
marthaurion <marthaurion@gmail.com>
2013-11-11 21:16:09 -0600

    added remaining face turns and created generic public functions for turning

--- src/Cube3.java
+++ src/Cube3.java
@@ -18,8 +18,37 @@ public class Cube3 {
 		return cube[fn.getValue()];
 	}
 	
+	//generic face turn for public use
+	public void turnFace(FaceName fn) {
+		if(fn == FaceName.UP) turnU();
+		else if(fn == FaceName.DOWN) turnD();
+		else if(fn == FaceName.FRONT) turnF();
+		else if(fn == FaceName.BACK) turnB();
+		else if(fn == FaceName.LEFT) turnL();
+		else if(fn == FaceName.RIGHT) turnR();
+		else {
+			System.out.println("SOMETHING WENT WRONG.");
+			System.exit(0);
+		}
+	}
+	
+	//turn a face twice
+	public void turnFace2(FaceName fn) {
+		turnFace(fn);
+		turnFace(fn);
+	}
+	
+	//turn a face counter-clockwise
+	public void turnCounter(FaceName fn) {
+		turnFace(fn);
+		turnFace(fn);
+		turnFace(fn);
+	}
+	
+	//private functions to turn faces
+	
 	//turns the top face clockwise
-	public void turnU() {
+	private void turnU() {
 		Color[] temp;
 		cube[0].rotate();
 		temp = cube[2].sendSide(FaceName.UP);
@@ -30,7 +59,7 @@ public class Cube3 {
 	}
 	
 	//turns the front face clockwise
-	public void turnF() {
+	private void turnF() {
 		Color[] temp;
 		cube[2].rotate();
 		
@@ -42,7 +71,7 @@ public class Cube3 {
 	}
 	
 	//turns the left face clockwise
-	public void turnL() {
+	private void turnL() {
 		Color[] temp;
 		cube[4].rotate();
 		
@@ -51,6 +80,42 @@ public class Cube3 {
 		cube[3].receiveSide(cube[1].sendSide(FaceName.RIGHT), FaceName.RIGHT);
 		cube[1].receiveSide(cube[2].sendSide(FaceName.LEFT), FaceName.RIGHT);
 		cube[2].receiveSide(temp, FaceName.LEFT);
+	}
+	
+	//turns the right face clockwise
+	private void turnR() {
+		Color[] temp;
+		cube[5].rotate();
+		
+		temp = cube[0].sendSide(FaceName.RIGHT);
+		cube[0].receiveSide(cube[2].sendSide(FaceName.RIGHT), FaceName.RIGHT);
+		cube[2].receiveSide(cube[1].sendSide(FaceName.LEFT), FaceName.RIGHT);
+		cube[1].receiveSide(cube[3].sendSide(FaceName.LEFT), FaceName.LEFT);
+		cube[3].receiveSide(temp, FaceName.LEFT);
+	}
+	
+	//turns the back face clockwise
+	private void turnB() {
+		Color[] temp;
+		cube[3].rotate();
+		
+		temp = cube[0].sendSide(FaceName.UP);
+		cube[0].receiveSide(cube[5].sendSide(FaceName.RIGHT), FaceName.UP);
+		cube[5].receiveSide(cube[1].sendSide(FaceName.UP), FaceName.RIGHT);
+		cube[1].receiveSide(cube[4].sendSide(FaceName.LEFT), FaceName.UP);
+		cube[4].receiveSide(temp, FaceName.LEFT);
+	}
+	
+	//turns the down face clockwise
+	private void turnD() {
+		Color[] temp;
+		cube[1].rotate();
+		
+		temp = cube[2].sendSide(FaceName.DOWN);
+		cube[2].receiveSide(cube[4].sendSide(FaceName.DOWN), FaceName.DOWN);
+		cube[4].receiveSide(cube[3].sendSide(FaceName.DOWN), FaceName.DOWN);
+		cube[3].receiveSide(cube[5].sendSide(FaceName.DOWN), FaceName.DOWN);
+		cube[5].receiveSide(temp, FaceName.DOWN);
 	}
 	
 	public void printCube() {
--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		Cube3 cube = new Cube3();
 		cube.printCube();
-		cube.turnU();
+		cube.turnFace(FaceName.UP);
 		System.out.println();
 		cube.printCube();
 	}
