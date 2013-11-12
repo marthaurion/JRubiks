4c536f7e06f3c0506c6d97aa24da3fb14e74265c
marthaurion <marthaurion@gmail.com>
2013-11-11 20:57:23 -0600

    add methods to turn left, top, and front faces

--- src/Cube3.java
+++ src/Cube3.java
@@ -18,13 +18,39 @@ public class Cube3 {
 		return cube[fn.getValue()];
 	}
 	
-	
-	public void turnFace(FaceName fn) {
-		Face3 focus = cube[fn.getValue()];
-		//need to grab the four surrounding faces too and move their sides along
+	//turns the top face clockwise
+	public void turnU() {
 		Color[] temp;
+		cube[0].rotate();
+		temp = cube[2].sendSide(FaceName.UP);
+		cube[2].receiveSide(cube[5].sendSide(FaceName.UP), FaceName.UP);
+		cube[5].receiveSide(cube[3].sendSide(FaceName.UP), FaceName.UP);
+		cube[3].receiveSide(cube[4].sendSide(FaceName.UP), FaceName.UP);
+		cube[4].receiveSide(temp, FaceName.UP);
+	}
+	
+	//turns the front face clockwise
+	public void turnF() {
+		Color[] temp;
+		cube[2].rotate();
 		
-		focus.rotate();
+		temp = cube[0].sendSide(FaceName.DOWN);
+		cube[0].receiveSide(cube[4].sendSide(FaceName.RIGHT), FaceName.DOWN);
+		cube[4].receiveSide(cube[1].sendSide(FaceName.DOWN), FaceName.RIGHT);
+		cube[1].receiveSide(cube[5].sendSide(FaceName.LEFT), FaceName.DOWN);
+		cube[5].receiveSide(temp, FaceName.LEFT);
+	}
+	
+	//turns the left face clockwise
+	public void turnL() {
+		Color[] temp;
+		cube[4].rotate();
+		
+		temp = cube[0].sendSide(FaceName.LEFT);
+		cube[0].receiveSide(cube[3].sendSide(FaceName.RIGHT), FaceName.LEFT);
+		cube[3].receiveSide(cube[1].sendSide(FaceName.RIGHT), FaceName.RIGHT);
+		cube[1].receiveSide(cube[2].sendSide(FaceName.LEFT), FaceName.RIGHT);
+		cube[2].receiveSide(temp, FaceName.LEFT);
 	}
 	
 	public void printCube() {
--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		Cube3 cube = new Cube3();
 		cube.printCube();
+		cube.turnU();
+		System.out.println();
+		cube.printCube();
 	}
 }
