7802cd7b9b9392ed6347ba579929f354d1d31b71
marthaurion <marthaurion@gmail.com>
2013-11-17 09:19:46 -0600

    added a test scramble and confirmed success

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		Cube cube = new Cube(3);
-		cube.runAlg("R U R\' U R U2 R\'");
-		//cube.runAlg("R L U D");
+		//cube.runAlg("R U R\' U R U2 R\'");
+		//cube.runAlg("D2 F2 L\' U\' L\' B2 U L\' D U\' R\' F\' U\' F D U\' R2 B\' D2 U\' F D2 B F2 D2");
 		cube.printCube();
 	}
 	
